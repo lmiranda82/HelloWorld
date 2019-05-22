39b6fcff346e0b6f6eed7fb75d1fc54c9321c358
“lmiranda82” <lr.miranda82@gmail.com>
2019-05-22 18:44:35 -0500

    added HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto solo es un mensaje de prueba");
+		for(int x=0; x<10; x++) {
+			System.out.println("Recorriendo un for:"+x);
+		}
+
+	}
+
+
+}
